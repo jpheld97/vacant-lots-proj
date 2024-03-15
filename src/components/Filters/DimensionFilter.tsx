4dc371d6dc5aab5c74950585c8f584c7073a7e92
Paul H Choi <paulhchoi+github@pm.me>
2024-03-15 14:37:10 -0400

    barrel export into root layout, explicit set client componenets

--- src/components/Filters/DimensionFilter.tsx
+++ src/components/Filters/DimensionFilter.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, FC } from "react";
 import { Button, Tooltip } from "@nextui-org/react";
 import { useFilter } from "@/context/FilterContext";
@@ -18,12 +20,12 @@ const DimensionFilter: FC<DimensionFilterProps> = ({
 }) => {
   const { dispatch, appFilter } = useFilter();
   const [selectedKeys, setSelectedKeys] = useState<string[]>(
-    appFilter[property]?.values || [],
+    appFilter[property]?.values || []
   );
 
   const toggleDimension = (dimension: string) => {
     const newSelectedKeys = selectedKeys.includes(dimension)
-      ? selectedKeys.filter((key) => key !== dimension)
+      ? selectedKeys.filter(key => key !== dimension)
       : [...selectedKeys, dimension];
     setSelectedKeys(newSelectedKeys);
     dispatch({
